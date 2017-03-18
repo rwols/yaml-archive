16cb279e26a72d31e451437de443df6f321b513a
Raoul Wols <raoulwols@gmail.com>
2017-03-18 22:08:49 +0100

    Document archive exception class

--- src/boost/archive/yaml_archive_exception.cpp
+++ src/boost/archive/yaml_archive_exception.cpp
@@ -1,12 +1,14 @@
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// yaml_archive_exception.cpp:
-
-// (C) Copyright 2009 Robert Ramey - http://www.rrsd.com .
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-//  See http://www.boost.org for updates, documentation, and revision history.
+/** @file
+ *
+ * @brief Defines an exception class for yaml archives.
+ *
+ * @author    Raoul Wols
+ *
+ * @date      2017
+ *
+ * @copyright See LICENSE.md
+ *
+ */
 
 #if (defined _MSC_VER) && (_MSC_VER == 1200)
 #pragma warning(disable : 4786) // too long name, harmless warning
