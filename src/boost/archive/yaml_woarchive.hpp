fda5ff58896be50f7ea23cde6afa6ed4a73e922e
Raoul Wols <raoulwols@gmail.com>
2017-03-18 21:51:16 +0100

    Document yaml_woarchive.hpp

--- src/boost/archive/yaml_woarchive.hpp
+++ src/boost/archive/yaml_woarchive.hpp
@@ -1,3 +1,15 @@
+/** @file
+ *
+ * @brief Defines wide concrete output archives.
+ *
+ * @author    Raoul Wols
+ *
+ * @date      2017
+ *
+ * @copyright See LICENSE.md
+ *
+ */
+
 #ifndef BOOST_ARCHIVE_YAML_WOARCHIVE_HPP
 #define BOOST_ARCHIVE_YAML_WOARCHIVE_HPP
 
@@ -5,16 +17,6 @@
 #if defined(_MSC_VER)
 #pragma once
 #endif
-
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// yaml_woarchive.hpp
-
-// (C) Copyright 2002 Robert Ramey - http://www.rrsd.com .
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-//  See http://www.boost.org for updates, documentation, and revision history.
 
 #include <boost/config.hpp>
 #ifdef BOOST_NO_STD_WSTREAMBUF
@@ -52,6 +54,13 @@ namespace detail {
 template <class Archive> class interface_oarchive;
 } // namespace detail
 
+/**
+ * @brief      YAML output archive for a wide character stream.
+ *
+ * Don't use this class directly. Instead, use boost::archive::yaml_woarchive.
+ *
+ * @tparam     Archive  The derived archive class.
+ */
 template <class Archive>
 class BOOST_SYMBOL_VISIBLE yaml_woarchive_impl
     : public basic_text_oprimitive<std::wostream>,
@@ -65,9 +74,6 @@ class BOOST_SYMBOL_VISIBLE yaml_woarchive_impl
     friend class basic_yaml_oarchive<Archive>;
     friend class save_access;
 #endif
-    // void end_preamble(){
-    //    basic_yaml_oarchive<Archive>::end_preamble();
-    //}
     template <class T> void save(const T& t)
     {
         basic_text_oprimitive<std::wostream>::save(t);
@@ -94,15 +100,60 @@ class BOOST_SYMBOL_VISIBLE yaml_woarchive_impl
     ~yaml_woarchive_impl();
 
   public:
+    /**
+     * @brief      Save bytes to the YAML archive.
+     *
+     * The binary data is realized as an unqouted base64 encoded string.
+     *
+     * @param[in]  address  The address
+     * @param[in]  count    The number of bytes
+     *
+     * @warning You should **never** use this method. Instead, use
+     * boost::serialization::make_binary_object. See the code example below.
+     *
+     * @code
+     * #include <boost/archive/yaml_woarchive.hpp>
+     * #include <boost/serialization/binary_object.hpp>
+     * #include <boost/serialization/nvp.hpp>
+     * #include <iostream>
+     * int main()
+     * {
+     *     char data[150];
+     *     boost::archive::yaml_woarchive yaml(std::wcout);
+     *     using boost::serialization::make_nvp;
+     *     using boost::serialization::make_binary_object;
+     *     yaml << make_nvp("data", make_binary_object(data, sizeof(data)));
+     *     return 0;
+     * }
+     * @endcode
+     */
     YAML_WARCHIVE_API void save_binary(const void* address, std::size_t count);
 };
 
-// we use the following because we can't use
-// typedef yaml_woarchive_impl<yaml_woarchive_impl<...> > yaml_woarchive;
-
-// do not derive from this class.  If you want to extend this functionality
-// via inhertance, derived from yaml_woarchive_impl instead.  This will
-// preserve correct static polymorphism.
+/**
+ * @brief      Concrete YAML output archive for a wide character stream.
+ *
+ * Use this class to output your classes to a YAML archive.
+ * Do not derive from this class.  If you want to extend this functionality via
+ * inheritance, derive from yaml_woarchive_impl instead. This will preserve
+ * correct static polymorphism.
+ *
+ * @see boost::archive::yaml_wiarchive for its input counter-part.
+ *
+ * @code
+ * #include <boost/archive/yaml_woarchive.hpp>
+ * #include <boost/serialization/nvp.hpp>
+ * #include <iostream>
+ * #include <string>
+ * int main()
+ * {
+ *     std::wstring world("world");
+ *     boost::archive::yaml_woarchive yaml(std::wcout);
+ *     yaml << boost::serialization::make_nvp("hello", world);
+ *     return 0;
+ * }
+ * @endcode
+ */
 #if BOOST_VERSION < 105600
 class BOOST_SYMBOL_VISIBLE yaml_woarchive
     : public yaml_woarchive_impl<yaml_woarchive>,
@@ -114,6 +165,15 @@ class BOOST_SYMBOL_VISIBLE yaml_woarchive
 {
 #endif
   public:
+    /**
+     * @brief      Constructor.
+     *
+     * @param      os     The output stream
+     * @param[in]  flags  Modifier flags
+     *
+     * @see http://www.boost.org/doc/libs/1_63_0/boost/archive/basic_archive.hpp
+     * for the available flags.
+     */
     yaml_woarchive(std::wostream& os, unsigned int flags = 0)
         : yaml_woarchive_impl<yaml_woarchive>(os, flags)
     {
